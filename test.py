3822663a9d02e71a68bd4b4614d9d104b60f70ac
prapunj <59813354+prapunj@users.noreply.github.com>
2021-08-13 11:03:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import urllib.request
+import json, re
+import pandas as pd
+
+
+def validate(xyz):
+    xyz.replace('"',"")
+    return re.sub(r"'", r'"', xyz)
+
 
 urls = ["http://localhost:8000/interfaces", "http://localhost:8000/interface/FastEthernet0/0"]
 
 for i in urls:
     req = urllib.request.urlopen(i)
-    print(req.read().decode())
+    output = req.read().decode()
+    output = re.findall(r'(\{.*?\})', output)
+    df = pd.DataFrame(map(lambda x: json.loads(validate(x)), output))
+    print(df)
